ba74f7d94272d45d1286bad0e03e66fae55b28b6
guilherme-donato-dev <donatocruzg@gmail.com>
2025-03-17 18:38:40 -0300

    funcionando

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title='Flix App - sua bíblia dos filmes!',
-    page_icon='🦆',
+
+def main():
+    st.set_page_config(
+        page_title='Flix App - sua bíblia dos filmes!',
+        page_icon='🦆',
+        )
+    st.title('Flix App')
+
+    menu_options = st.sidebar.selectbox(
+        'Selecione uma opção:',
+        {'Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações'}
     )
-st.title('Flix App')
-st.divider()
-title = st.text_input("Nome do filme: ", "A trança do careca")
-st.button('Salvar')
+
+    if menu_options == 'Início':
+        st.write('Início')
+
+    if menu_options == 'Gêneros':
+        st.write('Gêneros')
+
+    if menu_options == 'Atores/Atrizes':
+        st.write('Atores/Atrizes')
+
+    if menu_options == 'Filmes':
+        st.write('Filmes')
+
+    if menu_options == 'Avaliações':
+        st.write('Avaliações')    
+
+    st.divider()
+    st.text_input("Nome do filme: "),
+    st.button('Salvar')
+
+if __name__ == '__main__':
+    main()
